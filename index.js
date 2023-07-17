6537b9d19d630cefe37df3d19fa3aa57641a5ef5
hep <nanghnineiphyu2001@gmail.com>
2023-07-17 15:19:10 +0630

    update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 
 const port = 3000
 
-app.get("/get",(req,res)=>{
-    res.send("hello .")
-})
+// app.get("/get",(req,res)=>{
+//     res.send("hello .")
+// })
 
 app.listen(port,()=>{
     console.log("app is runing on port 3000")
